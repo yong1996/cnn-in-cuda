6f8c2733fd555405ffbeebc0bfc91a010aff2f72
yong1996 <yque@stevens.edu>
2021-04-07 17:18:46 -0400

    v.0

---
+++ cnn.cu
@@ -0,0 +1,26 @@
+// includes, system
+#include <string>
+
+// includes, kernels
+#include <cnn_kernel.cu>
+
+void loaddata(){
+    return 0;
+}
+
+void learn(){
+    return 0;
+}
+
+void test(){
+    return 0;
+}
+
+// cnn -- iteration, lr
+int main(int argc, char** argv) {
+    // loaddata();
+	learn();
+	test();
+
+	return 0;
+}
---
+++ cnn_kernel.cu
@@ -0,0 +1,4 @@
+__global__ void CnnMulKernel(Matrix M, Matrix N, Matrix P)
+{
+
+}
