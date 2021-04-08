3a04929e11fe5b39cf09b7f5411b6d75ddea7a42
JiruiWangDev <1036599554@qq.com>
2021-04-08 16:31:42 -0400

    CNN_layer stucture

---
+++ layer.h
@@ -0,0 +1,52 @@
+#include <cstdlib>
+#include <vector>
+#include <memory>
+#include <cublas_v2.h>
+#include <cuda.h>
+
+// includes, system
+#include <string>
+
+// includes, kernels
+#include <cnn_kernel.cu>
+
+
+
+#ifndef LAYER_H
+#define LAYER_H
+#endif
+
+
+const static float dt = 1.0E-01f;
+const static float threshold = 1.0E-02f;
+
+class CNN_layer{
+    private:
+        int width;
+        int height;
+        int depth;
+
+        float *bias;
+        float *weigth;
+    public:
+        CNN_layer(int in_width, int in_height, int in_depth);
+        ~CNN_layer();
+
+        void setOutput;
+        void clear;
+        void bp_clear;
+}
+
+CNN_layer::CNN_layer(int width, int height, int depth): width(in_width), height(in_height), depth(in_depth){
+
+}
+
+
+
+
+
+
+
+
+
+
--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@
 using namespace std;
 
 
-static void loadData(){
+static inline void loadData(){
     mnist_load("MNIST_data/train-images.idx3-ubyte", "data/train-labels.idx1-ubyte",
 		&train_set, &train_cnt);
 	mnist_load("MNIST_data/t10k-images.idx3-ubyte", "data/t10k-labels.idx1-ubyte",
