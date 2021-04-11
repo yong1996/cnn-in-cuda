190eae9e4f0d0db7134f59e615a05faee7db658c
JiruiWangDev <1036599554@qq.com>
2021-04-11 17:11:01 -0400

    layer to Layer

--- layer.h
+++ layer.h
@@ -20,24 +20,28 @@
 const static float dt = 1.0E-01f;
 const static float threshold = 1.0E-02f;
 
-class layer{
-    private:
+class Layer{
+    public:
         int width;
         int height;
-        int depth;
+        int number;
 
         float *bias;
         float *weigth;
-    public:
-        layer(int in_width, int in_height, int in_depth);
-        ~layer(); // free the memory allocation
+
+        float *d_output;
+        float *d_preact;
+        float *d_weight;
+
+        Layer(int in_width, int in_height, int in_number);
+        ~Layer(); // free the memory allocation
 
         void setOutput; // set data
         void clear;     //reset the GPU memory
         void bp_clear;  
 }
 
-layer::layer(int width, int height, int depth): width(in_width), height(in_height), depth(in_depth){
+Layer::Layer(int in_width, int in_height, int in_depth): width(in_width), height(in_height), depth(in_depth){
 
 }
 
