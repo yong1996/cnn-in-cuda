65833a0b9db6c444add9d0b69e6188e42697d100
JiruiWangDev <1036599554@qq.com>
2021-04-17 16:12:34 -0400

    layer

--- layer.cu
+++ layer.cu
@@ -1,6 +1,6 @@
 #include "layer.h"
 
-
+#define MASK_WIDTH = 3;
 
 // Layer constructor:
 Layer::Layer(int in_width, int in_height, int in_size): width(in_width), height(in_height), bytes(in_size){
@@ -67,5 +67,49 @@ __device__ float sigmoid(float v){
     return 1/(1 + exp(-v));
 }
 
+__global__ void step_function(float *input, float *output, const int N){
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    int size = blockDim.x * gridDim.x;
+    // TODO:
+
+}
+
+// convLayer 1 the weight is 6*3*3  output is 6*27*27
+__global__ void convLayer_forward(int C, int W_grid, int K, float *input[28][28], float output[6][27][27], float weight[6][3][3]){
+
+    
+    // int m, h, w, c, q, p;
+    // float Pvalue = 0;
+    // int n_start_point = i - (MASK_WIDTH/2);
+
+    // for (int m = 0; m < 6; m++){          // for each output feature map
+    //     for (int h = 0; h < 27; h++ ){
+    //         for(int w = 0; w < 27; w++){
+    //             output[m][h][w] = 0;
+    //             for (int c = 0; c < 1; c++){ // for each channel
+    //                 for (int p = 0; p < MASK_WIDTH; p++){
+    //                     for (int q = 0; q < MASK_WIDTH; q++){
+    //                         output[m][h][w] += input[h + p][w + q] * weight[m][p][q];
+    //                     }
+    //                 }
+    //             }
+    //         }
+
+    //     }
+
+    // }
+
+    int m = blockIdx.x;
+    int h =  blockIdx.y / W_grid  + threadIdx.y;
+    int w = blockIdx.y % W_grid + threadIdx.x;
+    float acc = 0.;
+    //for (int c = 0;  c < C; c++) {		// sum over all input channels, in this case, the channel is 1
+       for (int p = 0; p < K; p++)		// loop over KxK  filter
+          for (int q = 0; q < K; q++)  
+             acc += X[h+p][w+q] * W[m][p][q];
+    //}
+    output[m][h][w] = acc;
+
+}
 
 
