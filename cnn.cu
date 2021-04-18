774aefbe84d086ad6256efeb18fb70f3c766db3c
JiruiWangDev <1036599554@qq.com>
2021-04-17 20:57:07 -0400

    example

--- cnn.cu
+++ cnn.cu
@@ -1,29 +1,71 @@
+#define USE_MNIST_LOADER
+#define MNIST_DOUBLE
+
+
 // includes, system
 #include <string>
-
-// includes, kernels
-#include <cnn_kernel.cu>
 
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <vector>
 
+// //opencv for testing
+// #include <opencv2/core/core.hpp>
+// #include <opencv2/highgui/highgui.hpp>
+// using namespace cv;
+
 #include "mnist.h"
-#include "cnn.cpp"
+#include "layer.h"
+#include "util.h"
 
 
 //define the kernel size
 #define TILE_WIDTH = 4;  //for small example
 
 
+// set Layer
+static Layer l_input = Layer(0, 0, 28*28);
+static Layer l_c1 = Layer(5*5, 6, 24*24*6);
+static Layer l_s1 = Layer(4*4, 1, 6*6*6);
+static Layer l_f = Layer(6*6*6, 10, 10);
+
+static mnist_data *train_set, *test_set;
+static unsigned int train_cnt, test_cnt;
+
+
+static inline void loadData(){
+    mnist_load("MNIST_data/train-images.idx3-ubyte", "data/train-labels.idx1-ubyte",
+		&train_set, &train_cnt);
+	mnist_load("MNIST_data/t10k-images.idx3-ubyte", "data/t10k-labels.idx1-ubyte",
+		&test_set, &test_cnt);
+}
 
 
 
-void learn(){
+// void learn(){
+   
+// }
+
+// void test(){
+//     return 0;
+// }
+
+
+void forward(const double &data[28][28]){
+    float input[28][28];
+
+    for (int i = 0; i<28; i<++){
+        for (int j = 0; j<28; j++){
+            input[i][j] = data[i][j];
+        }
+    }
 
 
     //example for convLayer 1:
+
+    l_input.setInput((float *)input);
+
     int W_grid, H_grid;
     int W_out = 27, H_out = 27;
     int M = 6;  // The first (x) dimension in the grid maps to the M output feature maps
@@ -32,16 +74,16 @@ void learn(){
     Y = H_grid * W_grid; //The second (y) dimension in the grid maps to the tiles in the output feature maps
     dim3 blockDim(TILE_WIDTH, TILE_WIDTH, 1);
     dim3 gridDim(M, Y, 1);
-    ConvLayerForward_Kernel<<< gridDim, blockDim>>>(…);
+    // ConvLayerForward_Kernel<<< gridDim, blockDim>>>(int C = 1, W_grid, int K = 5, (float (*)[28])l_input.output,  (float (*)[24][24])l_c1.preact,(float (*)[5][5])l_c1.weight);
+    ConvLayerForward_Kernel<<< gridDim, blockDim>>>(int C = 1, W_grid, int K = 5, (float (*)[28])l_input.output,  (float (*)[24][24])l_c1.preact,(float (*)[5][5])l_c1.weight);
+    //apply_sigmoid <<<64,64>>>(l_c1.preact, l_c1.output, l_c1.size);
+    printf("%f", l_c1.preact[1]);
 
-     
-    
+    write_ppm("test.ppm",24,24,255,l_c1.preact[0]);
+
+                                               
+
 }
-
-void test(){
-    return 0;
-}
-
 // cnn -- iteration, lr
 // int main(int argc, char** argv) {
 //     // loaddata();
@@ -50,3 +92,12 @@ void test(){
 
 // 	return 0;
 // }
+
+
+
+
+int main(){
+    forward(train_set[0].data);
+
+    return 0;
+}
---
+++ util.h
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+#include <fcntl.h>
+#include <iostream>
+#include <cstdlib>
+#include <cassert>
+
+#include "string.h"
+
+
+void write_ppm(char* filename, int xsize, int ysize, int maxval, int* pic)
+{
+	FILE* fp;
+	//int x, y;
+
+	fp = fopen(filename, "wb");
+	if (!fp)
+	{
+		fprintf(stderr, "FAILED TO OPEN FILE '%s' for writing\n");
+		exit(-1);
+	}
+
+	fprintf(fp, "P6\n");
+	fprintf(fp, "%d %d\n%d\n", xsize, ysize, maxval);
+
+	int numpix = xsize * ysize;
+	for (int i = 0; i < numpix; i++) {
+		unsigned char uc = (unsigned char)pic[i];
+		fprintf(fp, "%c%c%c", uc, uc, uc);
+	}
+
+	fclose(fp);
+}
